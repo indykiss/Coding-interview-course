60cbf9bc98624a50099aacc2a56948476c82ef1e
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-09-23 15:18:31 -0400

    Update index.js

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -6,8 +6,27 @@
 // chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
-// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // We want to keep track of the new array
+  const arrChunks = [];
+  // We want to count through the array
+  let indexCount = 0;
+
+  // While we are looking at the entire array 
+  while(indexCount < array.length) {
+    // We are slicing the last parts of the array 
+    // that matches the desired chunk, using the 
+    // arr's index. Then adding it to new arr. 
+    // Then moving on with our counting.
+    arrChunks.push(array.slice(indexCount, indexCount + size));
+    indexCount += size;
+  }
+  return arrChunks
+}
+
+chunk([1, 2, 3, 4, 5], 2) 
+
+// --> [[ 1, 2, 3, 4, 5]]
 
 module.exports = chunk;
