ccf77d485f6be68e2188312e6d31e01285090e4f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-09-23 12:30:34 -0400

    Update index.js

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (var num = 1; num <= n; num++) {
+    if(num%3 === 0 && num%5 === 0) {
+      console.log("FizzBuzz")
+    }
+      else if(num%3 === 0){
+        console.log("Fizz")
+      }
+        else if(num%5 === 0){
+          console.log("Buzz")
+        }
+      else {
+        console.log(num)
+      }
+  }
+}
+
+fizzBuzz(16);
+
 
 module.exports = fizzBuzz;
