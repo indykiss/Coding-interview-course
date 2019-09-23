88f6b80a10e2255931fd5773ef4a284378998124
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-09-23 12:19:46 -0400

    Update index.js

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+  
+// Use for…of to iterate over the values in an iterable, like an array or a string. 
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    }
+      else {
+        charMap[char] = 1;
+      }
+  }
+
+// Use for…in to iterate over the properties of an object (the object keys). For..in also works for strings/ arrays, I think
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+return maxChar;
+}
+
+maxChar("aabc")
 
 module.exports = maxChar;
